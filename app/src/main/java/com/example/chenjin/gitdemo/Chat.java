179007924b61dff24048d58357119a86b88d4191
AttackXiaoJinJin <wwwcenjin1314@gmail.com>
2017-04-22 13:47:29 +0800

    Chat has completed

--- app/src/main/java/com/example/chenjin/gitdemo/Chat.java
+++ app/src/main/java/com/example/chenjin/gitdemo/Chat.java
@@ -1,14 +1,8 @@
 package com.example.chenjin.gitdemo;
 
-/*
+/**
  * Created by chenjin on 2017/4/22.
  */
 
 public class Chat {
-
-    public  static  void chat()
-    {
-        System.out.print("I'm chat.");
-    }
-
 }
